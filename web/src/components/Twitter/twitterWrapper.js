249fe7b094e19dc433ed5eedf8343bfef1519ca8
rachit2501 <rac.sri25@gmail.com>
2020-11-17 07:40:40 +0530

    basic layout

---
+++ web/src/components/Twitter/twitterWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Twitter from './twitter';
+
+export default function TwitterWrapper() {
+    return (
+        <div>
+            <Twitter/>
+        </div>
+    )
+}
---
+++ web/src/components/navigation.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Twitter from './Twitter/twitter';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" variant="fullWidth">
+          <Tab label="Tweets" {...a11yProps(0)} />
+          <Tab label="Articles" {...a11yProps(1)} />
+          <Tab label="Forum" {...a11yProps(2)} />
+          <Tab label="Information" {...a11yProps(3)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <Twitter/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Articles
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Forum
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Information
+      </TabPanel>
+    </div>
+  );
+}
