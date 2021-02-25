3774d6649072481b13bd87f68fb79f840482a985
rachit2501 <rac.sri25@gmail.com>
2021-02-25 22:02:42 +0530

    cors

--- web/server/index.js
+++ web/server/index.js
@@ -7,7 +7,14 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.static('public'));
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(allowCrossDomain);
 
 app.get('/twitter', async (req, res) => {
   firebase
